4e5f9bd54ad66207972b6232c2bc20ac78afc1b6
young971178 <young971178@gmail.com>
2026-07-19 11:28:41 +0900

    iris_histo.py 파일 추가

---
+++ iris_histo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+
+file_path = os.path.expanduser('~/Desktop/iris.csv')
+df = pd.read_csv(file_path, encoding='utf-8-sig')
+
+df.hist(figsize=(10, 8), layout=(2, 2), edgecolor='black')
+plt.tight_layout()
+plt.show()
