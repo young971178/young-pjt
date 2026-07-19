a0e06b89574a79a977d4032059091f4ac285c48c
young971178 <young971178@gmail.com>
2026-07-19 14:05:29 +0900

    Update iris_histo.py

--- iris_histo.py
+++ iris_histo.py
@@ -5,7 +5,7 @@ import os
 plt.rc('font', family='Malgun Gothic')
 plt.rcParams['axes.unicode_minus'] = False
 
-file_path = os.path.expanduser('~/Desktop/iris.csv')
+file_path = 'iris.csv'
 df = pd.read_csv(file_path, encoding='utf-8-sig')
 
 df.hist(figsize=(10, 8), layout=(2, 2), edgecolor='black')
